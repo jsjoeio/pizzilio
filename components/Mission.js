cfa0a07a303b8dd0728db6d16e4cc136e597f086
Joe Previte <jjprevite@gmail.com>
2018-10-06 19:17:42 -0700

    Add Mission component

---
+++ components/Mission.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Mission = styled.div`
+  h1 {
+    color: #fff;
+    font-size: 52px;
+  }
+
+  ul {
+    text-align: left;
+
+    li {
+      margin: 25px 0;
+    }
+  }
+`
+export default Mission
